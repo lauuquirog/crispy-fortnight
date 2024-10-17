d03230ec796bd17a77b7a7e9cd65184c93c07042
lauuquirog <quirogalaurajazmin@gmail.com>
2024-10-17 20:57:05 +0000

    djhsdjcl

---
+++ main.py
@@ -0,0 +1,16 @@
+nombre='Laura'
+numero=3
+booleano= True
+booleeano=False
+print(numero)
+print(3)
+edadDeLaura=15
+edadParaVotar=16
+puedeVotar= edadParaVotar<=edadDeLaura
+if puedeVotar:
+    print('puede votar')
+    while condicion:
+        contador=contador+1
+        condicion=contador<10
+        print(contador)
+    
